784539bb76583471c8693f7f59a02b877bb85ecb
sam-kmn <sancho.kmn@gmail.com>
2022-04-06 13:05:53 +0200

    Navbar styling

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import Search from './Search'
 import styles from '../../styles/Navbar.module.scss'
+import LinkStyles from '../../styles/Link.module.scss'
 
 const Navbar = () => {
 
   return (
     <nav className="navbar">
       <div className="container">
-        <a href={'/'} className={styles.brand}>QuickNews</a>
+        <a href={'/'} className={`${styles.brand} ${LinkStyles.Link}`}>QuickNews</a>
         <Search />
       </div>
     </nav>
